539efe45629935450d6beb6852e29e3beef2381f
Aidan <1016720@student.sae.edu.au>
2019-06-21 13:08:10 +1000

    Week 4 In Class Work

---
+++ Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameObject
+
+    public enum EnemyTypes
+    {
+        small,
+        medium,
+        large
+    }
+
+    public EnemyTypes myType;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public List<GameObject> enemyList;
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag('Enemy'))
+        {
+            enemyList.Add(enemy);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+
+
+    }
+
+    public void DamageEnemies()
+    {
+        foreach (GameObject enemy in enemyList)
+        {
+            enemy.GetComponent<Stats>().health -= 10;
+        }
+    }
+
+    public void HealEnemies()
+    {
+        foreach (GameObject enemy in enemyList)
+        {
+            enemy.GetComponent<Stats>().health += 10;
+        }
+    }
+
+    public void RemoveEnemy(GameObject enemyToRemove)
+    {
+        enemyList.Remove(enemyToRemove);
+    }
+
+}
